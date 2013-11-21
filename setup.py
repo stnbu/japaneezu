7f4213509c0fd3589cef87276dfb080e81067cc0
Mike Burr <mburr@unintuitive.org>
2013-11-21 19:29:24 +0900

    README, TODO, functional setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup_kwargs = {
+    'name': 'japaneezu',
+    'version': '0.0.1',
+    'description': 'A Very Beta Language Study Aide',
+    'author': 'Mike Burr',
+    'author_email': 'meburr@gmail.com',
+    'url': 'http://tbd.example.com',
+    'packages': ['japaneezu', 'japaneezu.tests'],
+}
+
+setup(**setup_kwargs)
